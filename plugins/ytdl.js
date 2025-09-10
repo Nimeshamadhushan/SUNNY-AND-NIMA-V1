8843664135c986fe80425b4abfe2b87ee48dc266
Nimeshamadhushan <89959527+Nimeshamadhushan@users.noreply.github.com>
2025-09-11 00:21:18 +0530

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -11,7 +11,7 @@ function replaceYouTubeID(url) {
 
 cmd({
     pattern: "play3",
-    alias: ["mp3", "ytmp3"],
+    alias: ["mp3", "song", "ytmp3"],
     react: "🎵",
     desc: "Download Ytmp3",
     category: "download",
